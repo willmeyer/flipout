9420378657a7a407d8294d0b3900b12faad53822
willmeyer <will@sptm.org>
2019-08-03 17:48:21 -0400

    getting sim

--- flipout.py
+++ flipout.py
@@ -6,21 +6,24 @@ import threading
 import math
 import datetime
 import random
-import poetry
-import clock
-import animation
-import rain
-
-from dcreemer.demo import animations
-from dcreemer.flipdot import client, display
-#from dcreemer.flipdot import sim
 import draw
+import curses
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 import text
 import transitions
 import utils
 import serial.tools.list_ports
 import os
+sys.path.append("..") # Adds higher directory to python modules path.
+sys.path.append("../dcreemer/flipdot") # Adds higher directory to python modules path.
+from dcreemer.flipdot import client, display
+from dcreemer.demo import animations
+from dcreemer.flipdot.sim import DisplaySim
+
+import poetry
+import clock
+import animation
+import rain
 
 d = display.Display(84, 14,
                     panels={
@@ -36,32 +39,11 @@ def run_job(job):
     print("Running job",job,"...")
     job.do()
 
-def run_sim():
-    s = DisplaySim(84, 14, panels={
-                             0: ((0, 0), (28, 7)),
-                             1: ((0, 7), (28, 7)),
-                             2: ((28, 0), (28, 7)),
-                             3: ((28, 7), (28, 7)),
-                             4: ((56, 0), (28, 7)),
-                             5: ((56, 7), (28, 7)),
-                            })
-    try:
-        sim.init_curses()
-        sim.start()
-        s.start_server()
-        try:
-            while True:
-                time.sleep(0.01)
-        except KeyboardInterrupt:
-            pass
-        s.stop()
-    finally:
-        sim.stop_curses()
-
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "udp":
+    if len(sys.argv) > 1 and sys.argv[1] == "sim-client":
         d.connect(client.UDPClient("localhost", 9999))
+    elif len(sys.argv) > 1 and sys.argv[1] == "sim-server":
+        exec(open("simulator.py").read());
     elif len(sys.argv) > 1:
         d.connect(client.SerialClient(sys.argv[1]))
     else:
